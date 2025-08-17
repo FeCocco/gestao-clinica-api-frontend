10c76600f51d8c005e796d5647cf7d189ef49529
FeCocco <felipegco@proton.me>
2025-08-17 18:26:18 -0300

    Some routing fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // <-- ADICIONE ESTA LINHA
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
