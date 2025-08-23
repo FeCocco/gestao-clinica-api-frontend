566d0672590fb8b128a924e645e0806f79343f71
FeCocco <felipegco@proton.me>
2025-08-23 04:24:06 -0300

    fix: Hero image adjustments for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,23 +8,31 @@ import heroImage2 from '../assets/hero2.jpg';
 import BotaoRoxo from "./BotaoRoxo/BotaoRoxo.jsx";
 
 function Hero(){
+
+    const ImageSwiper = () => (
+        <Swiper modules={[Autoplay, EffectFade]} effect={"fade"} loop={true} autoplay={{ delay: 4000, disableOnInteraction: false }} className="hero_swiper">
+            <SwiperSlide>
+                <img src={heroImage1} alt="Recepção confortável da clínica CliniCare" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={heroImage2} alt="Vista ampla da recepção da clínica" />
+            </SwiperSlide>
+        </Swiper>
+    )
+
     return (
         <div id="hero">
             <section className="hero_container">
                 <div className="hero_text_column">
                     <h1>A jornada para o seu sorriso perfeito começa aqui.</h1>
+                    <div className="hero_swiper_mobile">
+                        <ImageSwiper />
+                    </div>
                     <p>Equipamentos de ponta e especialistas dedicados a cuidar da sua saúde bucal com o carinho e a atenção que você merece.</p>
                     <BotaoRoxo/>
                 </div>
-                <div className="hero_image_column">
-                    <Swiper modules={[Autoplay, EffectFade]} effect={"fade"} loop={true} autoplay={{ delay: 4000, disableOnInteraction: false }} className="hero_swiper">
-                        <SwiperSlide>
-                            <img src={heroImage1} alt="Recepção confortável da clínica CliniCare" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src={heroImage2} alt="Vista ampla da recepção da clínica" />
-                        </SwiperSlide>
-                    </Swiper>
+                <div className="hero_image_column_desktop">
+                    <ImageSwiper />
                 </div>
             </section>
         </div>
